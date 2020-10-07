99a72025a264df11589380de4460af8fe675014f
dhmec <dharmoradiya@gmail.com>
2020-10-07 17:45:09 -0600

    Registered Menu in the functions.php

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+// Register a Navigation Menu for Header and Footer
+register_nav_menus( 
+    array( 
+        'main_menu' => 'Main Menu (Header)', 
+        'footer_menu' => 'Footer Menu' 
+    )
+);
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php get_header(); // header.php ?>
+
+<p>Welcome to Our Home Page!</p>
+
+<?php get_footer(); // footer.php
